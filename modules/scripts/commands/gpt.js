31deedf7e592ddf2428baebad08e752a333e3d80
nigfuhjfxtg <ffthy1678@gmail.com>
2025-01-22 15:39:38 +0100

    Update gpt.js

--- modules/scripts/commands/gpt.js
+++ modules/scripts/commands/gpt.js
@@ -1,3 +1,31 @@
+const axios = require('axios');
+const url = "https://www.blackbox.ai/api/chat";
+const data = {
+  id: "BNAU61V",
+  messages: [{ id: "BNAU61V", content: "مرحبا", role: "user" }],
+  agentMode: {},
+  validated: "00f37b34-a166-4efb-bce5-1312d87f2f94",
+};
+
+const headers = {
+  "Content-Type": "application/json",
+  Accept: "*/*",
+  Origin: "https://www.blackbox.ai",
+  Referer: "https://www.blackbox.ai/",
+  "User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+};
+
+axios
+  .post(url, data, { headers })
+  .then((response) => {
+    console.log("DATA :");
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.error("ERROR:");
+    console.error(error.response ? error.response.data : error.message);
+  });
 const { gpt } = require("gpti");
 
 module.exports.config = {
